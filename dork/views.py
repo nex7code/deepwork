17f704b6fa67b1115b22348a5adf4e00bd789a3c
nexome <nex7code@gmail.com>
2019-12-09 13:47:32 +0530

    All WORKBOOK and TASK completed

--- dork/views.py
+++ dork/views.py
@@ -8,34 +8,34 @@ from dork.forms import ProjectCreateForm, TaskCreateForm, SubTaskCreateForm
 
 class ProjectListView(generic.ListView):
 	model = Project
-	template_name = 'dork/project_list_view.html'
+	template_name = 'dork/project/project_list_view.html'
 
 
 class ProjectCreateView(generic.edit.CreateView):
 	model = Project
 	# fields = '__all__'
 	form_class = ProjectCreateForm
-	template_name = 'dork/project_create_view.html'
+	template_name = 'dork/project/project_create_view.html'
 	success_url = reverse_lazy('project-list-view')
 
 
 class ProjectDetailView(generic.DetailView):
 	model = Project
-	template_name = 'dork/project_detail_view.html'
+	template_name = 'dork/project/project_detail_view.html'
 
 
 class ProjectUpdateView(generic.edit.UpdateView):
 	model = Project
 	# fields = '__all__'#('title',)
 	form_class = ProjectCreateForm
-	template_name = 'dork/project_update_view.html'
+	template_name = 'dork/project/project_update_view.html'
 	success_url = reverse_lazy('project-list-view')
 
 
 class ProjectDeleteView(generic.edit.DeleteView):
 	model = Project
 	form_class = ProjectCreateForm
-	template_name = 'dork/project_delete_view.html'
+	template_name = 'dork/project/project_delete_view.html'
 	success_url = reverse_lazy('project-list-view')
 
 
@@ -43,29 +43,29 @@ class ProjectDeleteView(generic.edit.DeleteView):
 
 class TaskListView(generic.ListView):
 	model = Task
-	template_name = 'dork/task_list_view.html'
+	template_name = 'dork/task/task_list_view.html'
 
 
 class TaskCreateView(generic.edit.CreateView):
 	model = Task
 	form_class = TaskCreateForm
-	template_name = 'dork/task_create_view.html'
+	template_name = 'dork/task/task_create_view.html'
 	success_url = reverse_lazy('task-list-view')
 
 
 class TaskDetailView(generic.DetailView):
 	model = Task
-	template_name = 'dork/task_detail_view.html'
+	template_name = 'dork/task/task_detail_view.html'
 
 
 class TaskUpdateView(generic.edit.UpdateView):
 	model = Task
 	form_class = TaskCreateForm
-	template_name = 'dork/task_update_view.html'
+	template_name = 'dork/task/task_update_view.html'
 	success_url = reverse_lazy('task-list-view')
 
 
 class TaskDeleteView(generic.edit.DeleteView):
 	model = Task
-	template_name = 'dork/task_delete_view.html'
+	template_name = 'dork/task/task_delete_view.html'
 	success_url = reverse_lazy('task-list-view')
